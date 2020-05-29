213f448fa6a362b83d56a665438171c39837f0e5
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2020-05-29 07:59:08 -0400

    fixing async/dpc

--- nw-app.js
+++ nw-app.js
@@ -1,6 +1,7 @@
 const App = require("./app.js");
 const fs = require("fs");
 const path = require("path");
+const { dpc } = require("@aspectron/flow-async");
 
 
 false && chrome.developerPrivate.openDevTools({
