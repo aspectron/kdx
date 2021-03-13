7d34a06bc4e0905299d60eaf9c047f1467d9a0e0
surinder singh <surinder83singh@gmail.com>
2021-03-14 03:37:10 +0530

    dialog style, and kux path issue

--- modules/controller/kaspa-wallet-module.js
+++
@@ -1,8 +0,0 @@
-console.log("modulemodulemodule1111")
-const mod = require("@kaspa/wallet");
-console.log("modulemodulemodule2222")
-let keys = Object.keys(mod).join(", ")
-console.log("modulemodulemodule", keys)
-export const {
-	helper, Wallet, Storage, initKaspaFramework
-} = mod;
--- modules/controller/kaspa-wallet-worker-module.js
+++ modules/controller/kaspa-wallet-worker-module.js
@@ -1,5 +1,5 @@
 const mod = require("@kaspa/wallet-worker");
-let keys = Object.keys(mod).join(", ")
-console.log("sssssssssss keys:1:", keys)
+//let keys = Object.keys(mod).join(", ")
+//console.log("sssssssssss keys:1:", keys)
 export const {Wallet, initKaspaFramework, log, workerLog, Core, Storage, helper} = mod;
-console.log("sssssssssss keys:2:", keys)
+//console.log("sssssssssss keys:2:", keys)
