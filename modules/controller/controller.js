b8f4daa2c45f3f8dc710b3a0e9cfd9926de83905
surinder singh <surinder83singh@gmail.com>
2021-03-14 15:54:18 +0530

    hideQRScanner

--- modules/controller/controller.js
+++ modules/controller/controller.js
@@ -246,7 +246,7 @@ class KDXApp extends FlowApp{
 			<div style="height:192px;"></div>
 		</tab-content>
 		<tab-content for="wallet" class="wallet" data-active-display="flex">
-			<kaspa-wallet .walletMeta='${walletMeta}' hideNetwork hidefaucet></kaspa-wallet>
+			<kaspa-wallet .walletMeta='${walletMeta}' hideNetwork hidefaucet hideQRScanner></kaspa-wallet>
 		</tab-content>
 		<tab-content for="console" data-active-display="flex" class="vertical-flex term">
 			<flow-terminal id="kdx-console" class="x-terminal" background="#000" foreground="#FFF"></flow-terminal>
