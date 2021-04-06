30d001186e0b0f0ab2c76a46f1489916ab4cbb7c
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2021-04-06 14:09:14 -0400

    changing network type to the upcoming testnet4

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ class App extends FlowApp{
 	}
 
 	async initConfig(){
-		const currentNetworkType = 'testnet3';
+		const currentNetworkType = 'testnet4';
 
 		const networkTagFile = path.join(this.getConfigFolderPath(), '.network-type');
 		let reset = false;
