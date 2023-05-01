05e1587a63bc678a4b75bc775c68f6d6c935167f
HLXEasy <hlxeasy@gmail.com>
2023-05-01 23:17:17 +0200

    Formatted list of active languages for better maintainability

--- modules/controller/controller.js
+++ modules/controller/controller.js
@@ -401,7 +401,17 @@ class KDXApp extends FlowApp{
 		//console.log("entries", entries)
 		//let ce = new CustomEvent("flow-i18n-entries", {detail:{entries}})
 		//window.dispatchEvent(ce)
-		i18n.setActiveLanguages(['en', 'de', 'fr', 'id', 'it', 'pt_BR', 'ja', 'ko', 'zh']);
+		i18n.setActiveLanguages([
+			'en',
+			'de',
+			'fr',
+			'id',
+			'it',
+			'pt_BR',
+			'ja',
+			'ko',
+			'zh'
+		]);
 		i18n.setEntries(entries);
 		this.post("set-app-i18n-entries", {entries:i18n.getEntries()})
 		//i18n.setTesting(true);
